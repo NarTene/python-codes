a42b90cec4bd0c79da842e96073bc03d6d88a596
unknown <NarTene>
2023-03-03 14:21:18 -0500

    commit ec2_report_generator3.py

--- built-in-collection/ec2_report_generator3.py
+++ built-in-collection/ec2_report_generator3.py
@@ -1,9 +1,6 @@
 
-
-
 import boto3
 import logging
-
 from botocore.exceptions import ClientError
 import csv
 
@@ -11,17 +8,27 @@ import csv
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
 
-
 # constant global
 FILE_NAME = 'ec2_report.csv'
 
 
+# scan and retrieve all stopped Ec2 instances in us-east-1 region 
 def list_all_instances():
     # Call EC2 boto client
     ec2_client = boto3.client('ec2')
 
-    # Retrieve all Ec2 instance in us-east-1 region
-    response = ec2_client.describe_instances()
+    # filter and return only stopped Ec2 instances in us-east-1 region 
+    response = ec2_client.describe_instances(
+        Filters=[
+    {
+        'Name': 'instance-state-name',
+        'Values': [
+            'stopped'
+            
+        ]
+    },
+        ]
+    )
 
     # empty list
     lists = []
@@ -32,15 +39,65 @@ def list_all_instances():
             server_name = instance['Tags'][0]['Value']
             instance_id = instance['InstanceId']
             image_id = instance['ImageId']
+            volume_id = instance['BlockDeviceMappings'][0]['Ebs']['VolumeId']
             instance_type = instance['InstanceType']
 
             # add our data to the list
-            lists.append([server_name, instance_id, image_id, instance_type])
-            
-    
+            lists.append([server_name, instance_id, image_id, volume_id, instance_type])
     return lists
 
-def generate_excel_report(instances):
+# Create an EBS snapshot of that EC2
+def ebs_snapshot_stopped_ec2():
+
+#Creating a Snapshot from an EBS volume (with waiter)
+    AWS_REGION = "us-east-1"
+    ec2_resource = boto3.resource('ec2', region_name=AWS_REGION)
+     # empty list
+    snapshot_lists = []
+
+    stopped_ec2 = list_all_instances()
+    for m in stopped_ec2:
+        # create EBS snapshots 
+        snapshot = ec2_resource.create_snapshot(
+            VolumeId= m[3],
+            TagSpecifications=[
+                {
+                    'ResourceType': 'snapshot',
+                        'Tags': [
+                            {
+                                'Key': 'Name',
+                                'Value': 'boto3-created-EBS-snapshot'
+                             },
+                        ]
+                 },
+             ]
+        )
+        snapshot.wait_until_completed()
+        print(f'Snapshot {snapshot.id} created for volume {VOLUME_ID}')
+
+        snapshot_lists.append(snapshot.id)
+    return snapshot_lists
+
+# Terminate stopped ec2 instances
+def terminate_stopped_ec2():
+
+# Creating a client connection with AWS EC2 
+    ec2_clean = boto3.client('ec2')
+# Use response = ec2.start_instances( if you need to Start the Machine
+# Use response = ec2.terminate_instances( if you need to terminate the Machine
+
+# terminating the instance using terminate_instances.
+    for inst in list_all_instances():
+        response = ec2_clean.terminate_instances(
+	        InstanceIds=[
+            inst[1] #'i-03e3d79d5def39c75',
+        ],
+    )
+
+    print('all stopped instances were terminated successfully')
+
+# Generate a csv report
+def generate_csv_report(instances):
 
 
     ''' Generate an Excel Report
@@ -62,6 +119,8 @@ def generate_excel_report(instances):
         return False
     return True
 
+
+# Upload the csv report to s3 bucket
 def upload_report_to_s3():
 
     ''' Upload a file to an S3 bucket
@@ -113,7 +172,7 @@ def send_an_sns_email():
 
     # Publish to topic
     sns.publish(TopicArn=topic_arn, 
-        Message="an Ec2 report {FILE_NAME} updated to S3 bucket", 
+        Message="an Ec2 report {FILE_NAME} was updated to S3 bucket", 
         Subject="notification email from your SNS ")
     return True
 
@@ -124,9 +183,17 @@ if __name__ == '__main__':
     # retrieve data needed
     instances = list_all_instances()
     
+    # create ebs snapshots
+    snapshots = ebs_snapshot_stopped_ec2()
+
+    print(f"the list of created snapshots is :{snapshots}")
+
     # past the instance list to the function
-    generate_excel_report(instances)
+    generate_csv_report(instances)
     logger.info(f'Report: {FILE_NAME} has been generated succesfully!')
+
+    # terminate all instances
+    terminate_stopped_ec2()
 
     # upload to s3 bucket
     upload_report_to_s3()
@@ -134,4 +201,3 @@ if __name__ == '__main__':
 
     # call sns function
     send_an_sns_email()
-    logger.info(f'Report: {FILE_NAME} has been uploaded to S3 Bucket succesfully!')
